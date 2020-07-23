96e7a8f8edfd005bda32ce6ccda155b9ee4a876c
hendra5 <hendra@stockbit.com>
2020-07-23 18:43:01 +0700

    add shadow

--- app/theme/index.ts
+++ app/theme/index.ts
@@ -1,20 +1,24 @@
-import { lightColors, darkColors, ThemeColors } from './colors';
-import typography, { Typography } from './typography';
-import screen, { Screen } from './screen';
 import { DefaultTheme } from 'styled-components';
 
-export const spacing: Array<number> = [8, 16, 24, 32, 40, 56, 72, 80, 96, 120];
+import { lightColors, darkColors } from './colors';
+import typography from './typography';
+import screen from './screen';
+import shadows from './shadows';
+
+export const space: Array<number> = [8, 16, 24, 32, 40, 56, 72, 80, 96, 120];
 
 export const lightTheme: DefaultTheme = {
   colors: lightColors,
   typography,
   screen,
-  spacing,
+  space,
+  shadows,
 };
 
 export const darkTheme: DefaultTheme = {
   colors: darkColors,
   typography,
   screen,
-  spacing,
+  space,
+  shadows,
 };
--- styled.d.ts
+++ styled.d.ts
@@ -5,6 +5,7 @@ import { Theme } from './app/theme/index';
 import { ThemeColors } from './app/theme/colors';
 import { Typography } from './app/theme/typography';
 import { Screen } from './app/theme/screen';
+import { ShadowStyles } from './app/theme/shadows';
 
 // and extend them!
 declare module 'styled-components' {
@@ -12,6 +13,7 @@ declare module 'styled-components' {
     colors: ThemeColors;
     typography: Typography;
     screen: Screen;
-    spacing: Array<number>;
+    space: Array<number>;
+    shadows: ShadowStyles;
   }
 }
