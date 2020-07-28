dc5ed4e3333a3671dc3b3e5b8daf5dcbf1fe351a
hendra5 <hendra@stockbit.com>
2020-07-28 18:44:23 +0700

    change secondary font to primary

--- components/ArticleCard/Descriptions.ts
+++ components/ArticleCard/Descriptions.ts
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 
 const Descriptions = styled.div(
   ({ theme: { typography, colors, space } }) => css`
-    font-family: ${typography.fontFamily.secondary};
+    font-family: ${typography.fontFamily.primary};
     color: ${colors.text.body};
     padding: ${space[3]}px 0px;
   `
